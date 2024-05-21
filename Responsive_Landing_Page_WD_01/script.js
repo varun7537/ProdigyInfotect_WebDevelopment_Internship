bb2f6f00a9bb4edddba873f21eb09a3faddf1a06
varun7537 <111139401+varun7537@users.noreply.github.com>
2024-05-21 19:56:33 +0530

    Add files via upload

---
+++ Responsive_Landing_Page_WD_01/script.js
@@ -0,0 +1,15 @@
+function imgSlider(anything)
+{
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color)
+{
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+function changeTextColor(textColor) 
+{
+    document.querySelector('.text').style.color = textColor;
+}
